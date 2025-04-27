28e5dadf9ec94d93943b546f2887e194794a22d4
Agos <agosarias99@gmail.com>
2025-04-27 16:49:24 +0200

    change ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,4 +1,11 @@
 int strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t count;
 
+	count = 0;
+	while (count < n && s1[count] != '\0' && s2[count] != '\0' && s1[count] != s2[count])
+		count++;
+	if (s1[count] == s2[count])
+		return (0);
+	return (s1[count]-s2[count])
 }
