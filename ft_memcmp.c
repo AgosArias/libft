4a4c76ae73ac9c954dc77fd5e7d9f867c541555c
Agos <agosarias99@gmail.com>
2025-04-27 17:39:06 +0200

    add ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,11 +1,18 @@
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    
-    /*memcmp compara los primeros n bytes de las áreas de memoria s1 y s2. Devuelve:
+    int i;
 
-    0 si las áreas son iguales,
-    
-    un valor negativo si s1 es menor que s2,
-    
-    un valor positivo si s1 es mayor que s2.*/
+    i = 0;
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
+    {
+        i++;
+    }
+    if ((s1[i] != '\0' && s2[i] != '\0') || i == n)
+    {
+        return (s1[i] - s2[i]);
+    }
+    else
+    {
+        return (0);
+    }
 }
