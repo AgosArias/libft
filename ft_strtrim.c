92f5b58b36159adebbc6ccc0beed3d216fcda570
Agostina Arias <agosarias99@gmail.com>
2025-04-28 17:22:53 +0200

    add ft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,6 +5,7 @@ char *ft_strtrim(char const *s1, char const *set)
     char *w;
     int counter;
     int start;
+    int end;
 
     x = 0;
     counter = 1;
@@ -12,7 +13,7 @@ char *ft_strtrim(char const *s1, char const *set)
     {
         y = 0;
         counter = 0;
-        while ( set[y] != '\0')
+        while ( set[y] != '\0'  && counter != 1)
         {
             if (set[y] == s1[x])
                 counter ++;
@@ -22,5 +23,35 @@ char *ft_strtrim(char const *s1, char const *set)
     }
     if (s1[x] != '\0')
         start = x - 1;
+
+
+
+    while (s1[x] != '\0')
+        x++;
+    x--;
+
+
+
+
+    while (x != 0 && counter == 1)
+    {
+        y = 0;
+        counter = 0;
+        while ( set[y] != '\0'  && counter != 1)
+        {
+            if (set[y] == s1[x])
+                counter ++;
+            y++;
+        }
+        x--;
+    }
+    if (x != 0)
+        end = x + 1;
+    x = 0;
+    while (start + x != end || s1[start] != '\0')
+    {
+        w[x] = s1[start + x];
+        x++;
+    }
 
 }
