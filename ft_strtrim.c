8b373162a0f6d9ad191102383abbd9ec789c8ee8
Agostina Arias <agosarias99@gmail.com>
2025-04-28 16:30:44 +0200

    add ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,26 @@
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int x;
+    int y;
+    char *w;
+    int counter;
+    int start;
+
+    y = 0;
+    x = 0;
+    counter = 1;
+    while (s1[x] != '\0' && counter == 1)
+    {
+        counter = 0;
+        while ( set[y] != '\0')
+        {
+            if (set[y] == s1[x])
+                counter ++;
+            y++;
+        }
+        x++;
+    }
+    if (s1[x] != '\0')
+        start = x - 1;
+
+}
