102dd3739f0abfc59e602319b03368280f3b5bed
Agos <agosarias99@gmail.com>
2025-05-07 19:46:40 +0200

    add main

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
-#include 
+
+#include <stdio.h>
+#include "libft.h"
+
+int	main(void)
+{
+	char	str[] = "Hello, world!";
+	size_t	i = strlen(str);
+
+	printf("The length of the string is: %zu\n", i);
+	return (0);
+}
