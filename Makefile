aab6f1fa534948eb10463754f61304276859ebac
Agostina <agosarias99@gmail.com>
2025-05-16 19:22:39 +0200

    change and add in the makefile and lib the ft_memcpy.c

--- Makefile
+++ Makefile
@@ -14,6 +14,7 @@ SRC = ft_isalpha.c \
 	  ft_strlen.c \
 	  ft_memset.c \
 	  ft_bzero.c \
+	  ft_memcpy.c \
 	  
 
 OBJ = $(SRC:.c=.o)
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,14 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*memcpy(void *dest, const void *src, size_t n)
-{ 
-	int	i;
+#include "libft.h"
 
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{ 
+	size_t	i;
+	unsigned char *d;
+	unsigned char *s;
+
+	if (!dest && !src)
+        return (NULL);
+	d = (unsigned char *) dest;
+	s = (unsigned char *) src;
 	i = 0;
+	
 	while (n > i)
 	{
-		dest[i] = src[i];
+		d[i] = s[i];
 		i++;
 	}
+	return (dest);
 }
