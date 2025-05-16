2a1b9ed2dd7e1c810f9ee0c0614f5dad20971e77
Agostina <agosarias99@gmail.com>
2025-05-16 20:15:25 +0200

    change ft_put_fd funtions and add in the lib and makefile

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -19,7 +19,7 @@ void	ft_putstr_fd(char *s, int fd)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		ft_putstr_fd(s[i], fd);
+		ft_putstr_fd(&s[i], fd);
 		i++;
 	}
 }
--- libft.h
+++ libft.h
@@ -30,6 +30,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 
 #endif
