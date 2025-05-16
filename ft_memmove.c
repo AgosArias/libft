fb0954fe606828f0865201237955a94e91d29dd6
Agostina <agosarias99@gmail.com>
2025-05-16 19:56:17 +0200

    add changes in ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,17 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int	i;
+	size_t	i;
+	unsigned char *d;
+	unsigned char *s;
 
+	if (!dest && !src)
+        return (NULL);
+	d = (unsigned char*) dest;
+	s = (unsigned char*) src;
 	i = 0;
 	while (n > i)
 	{
-		dest[i] = src[i];
-		dest[i] = '\0';
+		d[i] = s[i];
 		i++;
 	}
+	return (dest);
 }
 
