08f227fd39b0b1f1a48e8b54335013a9b03dc255
Agostina Arias <agosarias99@gmail.com>
2025-05-26 10:24:58 +0200

    add ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -10,17 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strdup(char *src)
 {
 	int		i;
 	char	*c;
 
-	i = 0;
-	while (src[i] == '\0')
-		i++;
+	i = ft_strlen(src) + 1;
 	c = (char *) malloc (i * sizeof(char));
+	if (!c)
+		return (NULL);
 	i = 0;
 	while (src[i] != '\0')
 	{
@@ -30,22 +30,3 @@ char	*ft_strdup(char *src)
 	c[i] = src[i];
 	return (c);
 }
-
-/*char	*ft_strdup(char *src)
-{
-	int		i;
-	char	*c;
-
-	i = 0;
-	while (src[i] == '\0')
-		i++;
-	c = (char *) malloc (i * sizeof(char));
-	i = 0;
-	while (src[i] != '\0')
-	{
-		c[i] = src[i];
-		i++;
-	}
-	c[i] = src[i];
-	return (c);
-}*/
