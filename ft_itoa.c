d730ef39676911b96872b424ade1031d06a756cf
Agostina Arias <agosarias99@gmail.com>
2025-05-26 13:13:26 +0200

    change itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,9 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-int ft_lenght_int(int n)
+int ft_lenght(long n)
 {
     int i;
 
@@ -33,37 +32,25 @@ char    *ft_itoa(int n)
     int        i;
     int        sign;
 
-    if( n == -2147483648)
-    {
-        c = (char *)malloc(sizeof(char) * (12));
-        c[0] = '-';
-        i = 11;
-        while (0 < i)
-        {
-            c[i + 1] = (num % 10) + '0';
-            num = num / 10;
-            i--;
-        }    
-        return(c);
-    }
-
     sign = 0;
     num = n;
     if (num < 0)
     {
         sign++;
         num = -num;
+        i = ft_lenght(num) +1;
     }
-    i = ft_lenght_int(num);
-    c = (char *)malloc(sizeof(char) * (i + 1 + sign));
+    else
+        i = ft_lenght(num);
+    c = (char *)malloc(sizeof(char) * (i + 1 ));
     if (c == NULL)
         return (NULL);
     if (sign == 1)
         c[0] = '-';  
-    c[i-- +  sign] = '\0';
-    while (sign - 2 < i)
+    c[i] = '\0';
+    while (sign <= i - 1)
     {
-        c[i + sign] = (num % 10) + '0';
+        c[i -1] = (num % 10) + '0';
         num = num / 10;
         i--;
     }    
