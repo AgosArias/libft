5686b8191b4f1058b81b61c353ff0c8058027d7f
Agostina <agosarias99@gmail.com>
2025-05-29 17:18:25 +0200

    fix norminette

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,39 +6,47 @@
 /*   By: aarias-d <aarias-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:48:35 by aarias-d          #+#    #+#             */
-/*   Updated: 2025/05/07 18:10:04 by aarias-d         ###   ########.fr       */
+/*   Updated: 2025/05/29 17:16:25 by aarias-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_get_next_char(const char *str)
 {
-    int num;
-    int i;
-    int symbol;
+	int	i;
 
-    num = 0;
-    i = 0;    
-    while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-        i++;
-    symbol = 1;
-    if (str[i] == '-')
-    {
-        symbol = -1;
-        i++;
-    }
-    else if (str[i] == '+')
-        i++;
-    while (str[i] != '\0')
-    {        
-        if (ft_isdigit(str[i]) == 1 && num == 0)
-            num = str[i] - '0';
-        else if(ft_isdigit(str[i]) == 1  )
-            num = num * 10 + (str[i] - '0');
-        else
-            return(num*symbol);
-        i++;
-    }
-    return(num*symbol);
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	num;
+	int	i;
+	int	symbol;
+
+	num = 0;
+	i = ft_get_next_char(str);
+	symbol = 1;
+	if (str[i] == '-')
+	{
+		symbol = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0')
+	{
+		if (ft_isdigit(str[i]) == 1 && num == 0)
+			num = str[i] - '0';
+		else if (ft_isdigit(str[i]) == 1)
+			num = num * 10 + (str[i] - '0');
+		else
+			return (num * symbol);
+		i++;
+	}
+	return (num * symbol);
 }
--- libft.h
+++ libft.h
@@ -6,27 +6,27 @@
 /*   By: aarias-d <aarias-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 20:06:23 by aarias-d          #+#    #+#             */
-/*   Updated: 2025/05/29 16:14:20 by aarias-d         ###   ########.fr       */
+/*   Updated: 2025/05/29 16:57:27 by aarias-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stddef.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
 
-int     ft_isalpha(int c);
-int     ft_isdigit(int c);
-int     ft_isalnum(int c);
-int     ft_isascii(int c);
-int     ft_isprint(int c);
-int     ft_tolower(int c);
-int     ft_toupper(int c);
-int     ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t len);
@@ -37,8 +37,8 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
-size_t  ft_strlcat(char *dest, const char *src, size_t size);
-char    *ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_itoa(int n);
@@ -52,6 +52,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void    ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif
